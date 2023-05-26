981df3528fa8f4d4b621eb29b7e56f17875c8a2c
Elliott Bernstein <ebernstein@ripple.com>
2023-05-25 18:55:04 -0700

    get missing code in place

--- internal/handlers/thread/http.go
+++ internal/handlers/thread/http.go
@@ -18,38 +18,7 @@ func NewHandler(threadService ports.ThreadService) *Handler {
 func (h *Handler) Register(e *echo.Echo) {
 	e.GET("/threads/all", h.ListThreads)
 	e.GET("/threads/:id", h.GetThreadByID)
-	e.POST("/thread/create", h.CreateThread)
-	e.POST("/thread/reply", h.ThreadReply)
-}
-
-func (h *Handler) ThreadReply(c echo.Context) error {
-	values, err := c.FormParams()
-	if err != nil {
-		c.String(500, err.Error())
-		return err
-	}
-
-	threadID := values.Get("thread_id")
-	threadIDAsInt, err := strconv.Atoi(threadID)
-	if err != nil {
-		c.String(500, err.Error())
-		return err
-	}
-
-	author := values.Get("member_name")
-	body := values.Get("body")
-
-	ip := c.RealIP()
-
-	postID, err := h.threadService.NewPost(body, ip, author, threadIDAsInt)
-	if err != nil {
-		c.String(500, err.Error())
-		return err
-	}
-
-	return c.JSON(200, NewPostResponse{
-		PostID: postID,
-	})
+	e.GET("/thread/create", h.NewThread)
 }
 
 func (h *Handler) ListThreads(ctx echo.Context) error {
@@ -111,51 +80,23 @@ func (h *Handler) GetThreadByID(ctx echo.Context) error {
 //	return ctx.JSON(200, ID{id})
 //}
 
-func (h *Handler) CreateThread(c echo.Context) error {
-	body := c.FormValue("body")
-	subject := c.FormValue("subject")
-
-	ip := c.RealIP()
-
-	author := c.FormValue("member")
-
-	threadID, err := h.threadService.NewThread(author, ip, body, subject)
+func (h *Handler) NewThread(c echo.Context) error {
+	thread := &Thread{}
+	err := c.Bind(thread)
 	if err != nil {
-		c.String(500, err.Error())
+		c.JSON(400, ErrorResponse{Message: err.Error()})
 		return err
 	}
 
-	return c.JSON(200, NewThreadResponse{
-		ThreadID: threadID,
-	})
-}
+	id, err := h.threadService.NewThread(strconv.Itoa(thread.MemberID), thread.MemberIP, thread.FirstPostText, thread.Subject)
+	if err != nil {
+		c.JSON(500, ErrorResponse{Message: err.Error()})
+		return err
+	}
 
-//
-//func (h *Handler) NewThread(c echo.Context) error {
-//	thread := &Thread{}
-//	err := c.Bind(thread)
-//	if err != nil {
-//		c.JSON(400, ErrorResponse{Message: err.Error()})
-//		return err
-//	}
-//
-//	id, err := h.threadService.NewThread(strconv.Itoa(thread.MemberID), thread.MemberIP, thread.FirstPostText, thread.Subject)
-//	if err != nil {
-//		c.JSON(500, ErrorResponse{Message: err.Error()})
-//		return err
-//	}
-//
-//	return c.JSON(200, ID{ID: id})
-//}
+	return c.JSON(200, ID{ID: id})
+}
 
 type ErrorResponse struct {
 	Message string `json:"message"`
-}
-
-type NewPostResponse struct {
-	PostID int `json:"post_id"`
-}
-
-type NewThreadResponse struct {
-	ThreadID int `json:"thread_id"`
 }
