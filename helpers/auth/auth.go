26e3ae0161e804f3e32c82db9ed7cce5465beac1
Michael Byrne <michaelbyrne@Michaels-MacBook-Pro-3.local>
2023-06-25 13:42:29 -0700

    cleaned up comments in auth file

--- helpers/auth/auth.go
+++ helpers/auth/auth.go
@@ -17,8 +17,6 @@ func GetJWTSecret() string {
 	return jwtSecretKey
 }
 
-// Create a struct that will be encoded to a JWT.
-// We add jwt.StandardClaims as an embedded type, to provide fields like expiry time.
 type Claims struct {
 	Name string `json:"name"`
 	v5claims.RegisteredClaims
@@ -28,7 +26,6 @@ func GetJWTClaims(c echo.Context) v5claims.Claims {
 	return Claims{}
 }
 
-// GenerateTokensAndSetCookies generates jwt token and saves it to the http-only cookie.
 func GenerateTokensAndSetCookies(member *domain.Member, c echo.Context, timeout time.Duration) error {
 	accessToken, exp, err := generateAccessToken(member, timeout)
 	if err != nil {
@@ -42,15 +39,12 @@ func GenerateTokensAndSetCookies(member *domain.Member, c echo.Context, timeout 
 }
 
 func generateAccessToken(member *domain.Member, timeout time.Duration) (string, time.Time, error) {
-	// Declare the expiration time of the token (1h).
 	expirationTime := time.Now().Add(1 * timeout)
 
 	return generateToken(member, expirationTime, []byte(GetJWTSecret()))
 }
 
-// Pay attention to this function. It holds the main JWT token generation logic.
 func generateToken(member *domain.Member, expirationTime time.Time, secret []byte) (string, time.Time, error) {
-	// Create the JWT claims, which includes the username and expiry time.
 	claims := &Claims{
 		Name: member.Name,
 		RegisteredClaims: v5claims.RegisteredClaims{
@@ -59,10 +53,8 @@ func generateToken(member *domain.Member, expirationTime time.Time, secret []byt
 		},
 	}
 
-	// Declare the token with the HS256 algorithm used for signing, and the claims.
 	token := v5claims.NewWithClaims(v5claims.SigningMethodHS256, claims)
 
-	// Create the JWT string.
 	tokenString, err := token.SignedString(secret)
 	if err != nil {
 		return "", time.Now(), err
@@ -70,20 +62,17 @@ func generateToken(member *domain.Member, expirationTime time.Time, secret []byt
 	return tokenString, expirationTime, nil
 }
 
-// Here we are creating a new cookie, which will store the valid JWT token.
 func setTokenCookie(name, token string, expiration time.Time, c echo.Context) {
 	cookie := new(http.Cookie)
 	cookie.Name = name
 	cookie.Value = token
 	cookie.Expires = expiration
 	cookie.Path = "/"
-	// Http-only helps mitigate the risk of client side script accessing the protected cookie.
 	cookie.HttpOnly = true
 
 	c.SetCookie(cookie)
 }
 
-// Purpose of this cookie is to store the user's name.
 func setUserCookie(member *domain.Member, expiration time.Time, c echo.Context) {
 	cookie := new(http.Cookie)
 	cookie.Name = "user"
@@ -93,8 +82,6 @@ func setUserCookie(member *domain.Member, expiration time.Time, c echo.Context) 
 	c.SetCookie(cookie)
 }
 
-// JWTErrorChecker will be executed when user try to access a protected path.
 func JWTErrorChecker(c echo.Context, err error) error {
-	// Redirects to the signIn form.
 	return c.Redirect(http.StatusMovedPermanently, c.Echo().Reverse("login"))
 }
