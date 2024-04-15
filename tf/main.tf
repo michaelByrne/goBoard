006573d943084de139efbf25c35776de307a3741
Michael Byrne <michaelbyrne@Michaels-MacBook-Pro-3.local>
2024-04-15 19:21:44 -0400

    fmt again

--- tf/main.tf
+++ tf/main.tf
@@ -327,8 +327,8 @@ provider "docker" {
 }
 
 module "oidc_github" {
-  source  = "unfunco/oidc-github/aws"
-  version = "1.7.1"
+  source              = "unfunco/oidc-github/aws"
+  version             = "1.7.1"
   attach_admin_policy = true
 
   github_repositories = [
